a08e1a0ada1a4d34b6fc0366bbe4057e47a7b940
ibaguio <baguio.ivan@gmail.com>
2017-04-04 17:45:41 +0800

    updated django admin

--- django_dragonpay/admin.py
+++ django_dragonpay/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from django_dragonpay.models import *
 
 
+def readable_amount(obj):
+    return "{} {:,}".format(obj.currency, obj.amount)
+
+
 class StatusPayoutsFilter(admin.SimpleListFilter):
+    '''Admin filter for Payouts Status'''
     title = 'Status'
     parameter_name = 'status'
 
@@ -20,12 +25,15 @@ class PayoutUserAdmin(admin.ModelAdmin):
 
 
 class TransactionAdmin(admin.ModelAdmin):
+    date_heirarchy = 'created_at'
     search_fields = ('txn_id', 'email')
     list_filter = (StatusPayoutsFilter, )
+    list_display = ('token', 'status', readable_amount, 'description', 'email')
 
 
 class PayoutsAdmin(admin.ModelAdmin):
     class CompletedPayoutsFilter(admin.SimpleListFilter):
+        '''Admin filter for Completed Payouts.'''
         title = 'Completed'
         parameter_name = 'completed'
 
@@ -43,8 +51,11 @@ class PayoutsAdmin(admin.ModelAdmin):
                 # filter for Pending/In progress
                 return queryset.filter(status__in=['P', 'G'])
 
+    date_heirarchy = 'created_at'
     search_fields = ('txn_id', 'user_id', 'user_name', 'email')
     list_filter = (CompletedPayoutsFilter, StatusPayoutsFilter)
+    list_display = (
+        'txn_id', 'user_name', 'email', readable_amount, 'description')
 
 
 admin.site.register(DragonpayPayoutUser, PayoutUserAdmin)
